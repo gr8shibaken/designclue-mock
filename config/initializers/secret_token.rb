e96a39c068222c8324132b11a69b96bb7b035ff0
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2012-03-01 10:37:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Designclue::Application.config.secret_token = '1330ec44923eccb2d7f34686a7e83221210498250e72d51e393549a016d2c7cd92c9084dd09af5fb85ac7bcc47354acaadb2a680fab4739162cb60cbdb0a88c3'
