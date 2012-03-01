4f69614c8f84ad24a09ab07bdbb2a7a3686dd1b3
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2012-03-01 10:53:19 +0900

    add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  
+  USERNAME = "admin"
+  PASSWORD = "purplecow"
+
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |name,password|
+      (@name = name) == USERNAME && password == PASSWORD
+      end
+  end
 end
